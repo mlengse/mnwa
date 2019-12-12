23d224924430685bd9c4331da2d0857bbd0395d2
mlengse <anjangkn@gmail.com>
2019-12-12 10:48:16 +0700

    asd

--- core.js
+++ core.js
@@ -1,85 +1,26 @@
-const pptr = require('puppeteer-core')
-const ora = require('ora');
-const qrcode = require('qrcode-terminal')
-const { from, merge } = require('rxjs')
-const { take } = require('rxjs/operators')
 const path = require('path')
+const pptr = require('puppeteer-core')
 const { Whatsapp } = require('./api/whatsapp')
-const spinner = ora({
-  stream: process.stdout
-});
+const Util = require('./util')
 
-module.exports = class Core {
+module.exports = class Core  extends Util {
+
   constructor(config){
-    this.config = config
+    super(config)
   }
 
   async close(){
     await this.browser.close()
   }
 
-  async init(){
-    spinner.start('Initializing whatsapp');
-
-    this.browser = await pptr.launch(this.config.pptrOpt);
-    this.pages = await this.browser.pages()
-    this.page = this.pages[0]
-
-    await this.page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-
-    await this.page.setRequestInterception(true);
-    
-    this.page.on('request', (request) => {
-      request.continue();
-    });
-  
-    await this.page.goto('https://web.whatsapp.com', this.config.waitOpt)
-  
-    spinner.succeed();
-
-    spinner.start('Authenticating');
-
-    const authenticated = await this.isAuthenticated();
-  
-    // If not authenticated, show QR and wait for scan
-    if (authenticated) {
-      spinner.succeed();
-    } else {
-      spinner.info('Authenticate to continue');
-      await this.retrieveQR();
-  
-      // Wait til inside chat
-      await this.isInsideChat().toPromise();
-      spinner.succeed();
-    }
-  
-    spinner.start('Injecting api');
-    
-    await this.page.addScriptTag({
-      path: require.resolve(path.join(__dirname, './lib', 'wapi.js'))
-    });
-    await this.page.addScriptTag({
-      path: require.resolve(path.join(__dirname, './lib', 'middleware.js'))
-    });
-  
-    
-    spinner.succeed();
-  
-    spinner.succeed('Whatsapp is ready');
-  
-    this.client = new Whatsapp(this.page);
-
-    return this.client
-  }
-
   isAuthenticated() {
-    return merge(this.needsToScan(), this.isInsideChat())
-      .pipe(take(1))
+    return this.merge(this.needsToScan(), this.isInsideChat())
+      .pipe(this.take(1))
       .toPromise();
   };
   
   needsToScan() {
-    return from(
+    return this.from(
       this.page
         .waitForSelector('body > div > div > .landing-wrapper', {
           timeout: 0
@@ -89,7 +30,7 @@ module.exports = class Core {
   };
   
   isInsideChat() {
-    return from(
+    return this.from(
       this.page
         .waitForFunction(
           `
@@ -106,19 +47,94 @@ module.exports = class Core {
   };
   
   async retrieveQR() {
-    spinner.start('Loading QR');
+    this.spinner.start('Loading QR');
     await this.page.waitFor(5000)
     await this.page.screenshot({path: 'buddy-screenshot.png'});
     await this.page.waitForSelector("img[alt='Scan me!']", { timeout: 0 });
     const qrImage = await this.page.evaluate(
       `document.querySelector("img[alt='Scan me!']").parentElement.getAttribute("data-ref")`
     );
-    spinner.succeed();
-    qrcode.generate(qrImage, {
+    this.spinner.succeed();
+    this.qrcode.generate(qrImage, {
       small: true
     });
   
     return true;
+  }
+
+  async getUnreadMessagesInChat(unread){
+    this.spinner.start(`get unread messages in chat`);
+
+    let messages = []
+
+    while(!messages || !messages.length || messages.length < unread.unreadCount) {
+      messages = await this.page.evaluate( id => {
+        return WAPI.getAllMessagesInChat(id)
+      }, unread.id)
+    }
+
+    while( messages.length > unread.unreadCount){
+      messages.shift()
+    }
+
+    this.spinner.succeed()
+
+    return messages
+
+  }
+
+  async init(){
+    this.spinner.start('Initializing whatsapp');
+
+    this.browser = await pptr.launch(this.config.pptrOpt);
+    this.pages = await this.browser.pages()
+    this.page = this.pages[0]
+
+    await this.page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+
+    await this.page.setRequestInterception(true);
+    
+    this.page.on('request', (request) => {
+      request.continue();
+    });
+  
+    await this.page.goto('https://web.whatsapp.com', this.config.waitOpt)
+  
+    this.spinner.succeed();
+
+    this.spinner.start('Authenticating');
+
+    const authenticated = await this.isAuthenticated();
+  
+    // If not authenticated, show QR and wait for scan
+    if (authenticated) {
+      this.spinner.succeed();
+    } else {
+      this.spinner.info('Authenticate to continue');
+      await this.retrieveQR();
+  
+      // Wait til inside chat
+      await this.isInsideChat().toPromise();
+      this.spinner.succeed();
+    }
+  
+    this.spinner.start('Injecting api');
+    
+    await this.page.addScriptTag({
+      path: require.resolve(path.join(__dirname, './lib', 'wapi.js'))
+    });
+    await this.page.addScriptTag({
+      path: require.resolve(path.join(__dirname, './lib', 'middleware.js'))
+    });
+  
+    
+    this.spinner.succeed();
+  
+    this.spinner.succeed('Whatsapp is ready');
+  
+    this.client = new Whatsapp(this.page);
+
+    return this.client
   }
 
 }
--- index.js
+++ index.js
@@ -2,50 +2,25 @@ require('dotenv').config()
 
 const Core = require('./core')
 const config = require('./config')
-const time = s => new Date(s * 1e3).toISOString()
 const wa = new Core(config)
 ;(async()=>{
   try{
     const client = await wa.init()
 
-
-    const handleMessage = async message => {
-			console.log('-------------')
-			
-			let msg = {
-				time: time(message.t),
-				grup: message.isGroupMsg ? message.chat.name : undefined,
-				pengirim: message.sender.name || message.sender.pushname || message.sender.shortName || message.sender.formattedName || message.sender.id.user,
-				jenis: message.broadcast ? 'status' : 'chat',
-				isi: message.type === 'chat' ? message.body : `${message.type} | ${message.mimetype}`,
-				quote: message.quotedMsg ? message.quotedMsg.type === 'chat' ? message.quotedMsg.body : `${message.quotedMsg.type} | ${message.quotedMsg.mimetype}` : undefined,
-				// str: JSON.stringify(message)
-			}
-
-			console.log(JSON.parse(JSON.stringify(msg)))
-
-    }
-
-		client.onMessage( handleMessage )
+		client.onMessage( async msg => {
+			let ms = await handleMessage(msg)
+			console.log(ms) 
+		})
 
 		let unreads = await client.getAllChats(true)
 
     for(let unread of unreads){
 
-			let messages = []
-
-    	while(!messages || !messages.length || messages.length < unread.unreadCount) {
-    		messages = await wa.page.evaluate( id => {
-	    		return WAPI.getAllMessagesInChat(id)
-	    	}, unread.id)
-    	}
-
-    	while( messages.length > unread.unreadCount){
-    		messages.shift()
-    	}
+			let messages = await wa.getUnreadMessagesInChat(unread)
 
     	for(let message of messages){
-    		await handleMessage(message)
+				let ms = await wa.handleMessage(message)
+				console.log(ms)
     	}
     	
     }
