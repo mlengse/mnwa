dd66b57f8a8152907dc64ae70dda0936d375522e
mlengse <anjangkn@gmail.com>
2020-01-27 12:07:51 +0700

    zcsdf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ moment.locale('id')
 let subscriber = null
 if(process.env.REDIS_HOST){
   subscriber = redis.createClient({
-    host: process.env.REDIS_HOST
+    // host: process.env.REDIS_HOST
   })
 }
 
