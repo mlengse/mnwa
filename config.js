f19c9c60f7ad2b2748b70fd7a886633bf2b678c3
mlengse <anjangkn@gmail.com>
2020-06-04 12:59:43 +0700

    20200604

--- config.js
+++ config.js
@@ -43,11 +43,8 @@ if(process.platform === 'win32' || CHROME_PATH ) {
   pptrOpt = {
     // headless: true,
     headless: false,
-    // userDataDir: './tmp',
-    // executablePath: "/usr/bin/google-chrome-stable",
-  
     executablePath: `${CHROME_PATH}`, 
-    userDataDir: USER_DATA_PATH,
+    userDataDir: `${USER_DATA_PATH}`,
     // args: [
     //   '--content-shell-hide-toolbar',
     //   '--hide',
