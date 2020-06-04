24e994fc0c85acc69b12ff9593aae4e8056468f6
mlengse <anjangkn@gmail.com>
2020-06-04 12:20:46 +0700

    20200604

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const {
   USER_DATA_PATH,
   RM_REGEX,
+  CHROME_PATH
 } = process.env
 
 const keywords = ['tes', 'cek', 'cari', 'daftar']
@@ -36,47 +37,75 @@ const waitOpt = {
   waitUntil: 'networkidle2'
 }
 
-const pptrOpt = {
-  // headless: true,
-  headless: false,
-  userDataDir: USER_DATA_PATH,
-  // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-  executablePath: "/usr/bin/google-chrome-stable",
-  // args: ['--no-sandbox', '--disable-setuid-sandbox', '--auto-open-devtools-for-tabs' ]
-  // args: [
-  //   // `--app=${whatsappUrl}`,
-  //   '--log-level=3', // fatal only
-  //   //'--start-maximized',
-  //   '--no-default-browser-check',
-  //   '--disable-site-isolation-trials',
-  //   '--no-experiments',
-  //   '--ignore-gpu-blacklist',
-  //   '--ignore-certificate-errors',
-  //   '--ignore-certificate-errors-spki-list',
-  //   '--disable-gpu',
-  //   '--disable-extensions',
-  //   '--disable-default-apps',
-  //   '--enable-features=NetworkService',
-    // '--disable-dev-shm-usage',
-    // '--disable-setuid-sandbox',
-    // '--no-sandbox',
-  //   // Extras
-  //   '--disable-webgl',
-  //   '--disable-threaded-animation',
-  //   '--disable-threaded-scrolling',
-  //   '--disable-in-process-stack-traces',
-  //   '--disable-histogram-customizer',
-  //   '--disable-gl-extensions',
-  //   '--disable-composited-antialiasing',
-  //   '--disable-canvas-aa',
-  //   '--disable-3d-apis',
-  //   '--disable-accelerated-2d-canvas',
-  //   '--disable-accelerated-jpeg-decoding',
-  //   '--disable-accelerated-mjpeg-decode',
-  //   '--disable-app-list-dismiss-on-blur',
-  //   '--disable-accelerated-video-decode'
-  // ]
+let pptrOpt = {}
+// console.log(process.platform)
+if(process.platform === 'win32' || CHROME_PATH ) {
+  pptrOpt = {
+    headless: true,
+    // headless: false,
+    executablePath: `${CHROME_PATH}`, 
+    userDataDir: `${USER_DATA_PATH}`,
+    // args: [
+    //   '--content-shell-hide-toolbar',
+    //   '--hide',
+    //   '--hide-scrollbars',
+    //   '--window-position=0,0',
+    //   '--window-size=0,0'
+    // ]
+  }
+} else {
+  pptrOpt = {
+    headless: true,
+    userDataDir: `${USER_DATA_PATH}`,
+    args: [
+      '--no-sandbox', 
+      '--disable-setuid-sandbox', 
+    ]
+  }
 }
+
+
+// const pptrOpt = {
+//   // headless: true,
+//   headless: false,
+//   userDataDir: USER_DATA_PATH,
+//   // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+//   executablePath: "/usr/bin/google-chrome-stable",
+//   // args: ['--no-sandbox', '--disable-setuid-sandbox', '--auto-open-devtools-for-tabs' ]
+//   // args: [
+//   //   // `--app=${whatsappUrl}`,
+//   //   '--log-level=3', // fatal only
+//   //   //'--start-maximized',
+//   //   '--no-default-browser-check',
+//   //   '--disable-site-isolation-trials',
+//   //   '--no-experiments',
+//   //   '--ignore-gpu-blacklist',
+//   //   '--ignore-certificate-errors',
+//   //   '--ignore-certificate-errors-spki-list',
+//   //   '--disable-gpu',
+//   //   '--disable-extensions',
+//   //   '--disable-default-apps',
+//   //   '--enable-features=NetworkService',
+//     // '--disable-dev-shm-usage',
+//     // '--disable-setuid-sandbox',
+//     // '--no-sandbox',
+//   //   // Extras
+//   //   '--disable-webgl',
+//   //   '--disable-threaded-animation',
+//   //   '--disable-threaded-scrolling',
+//   //   '--disable-in-process-stack-traces',
+//   //   '--disable-histogram-customizer',
+//   //   '--disable-gl-extensions',
+//   //   '--disable-composited-antialiasing',
+//   //   '--disable-canvas-aa',
+//   //   '--disable-3d-apis',
+//   //   '--disable-accelerated-2d-canvas',
+//   //   '--disable-accelerated-jpeg-decoding',
+//   //   '--disable-accelerated-mjpeg-decode',
+//   //   '--disable-app-list-dismiss-on-blur',
+//   //   '--disable-accelerated-video-decode'
+//   // ]
+// }
 
 
 
