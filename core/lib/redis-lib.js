6273091f461fbeba74d6e3dadb7c8ebdfe1f4dda
mlengse <anjangkn@gmail.com>
2021-03-23 11:56:25 +0700

    fghfh

--- core/lib/redis-lib.js
+++ core/lib/redis-lib.js
@@ -1,10 +1,17 @@
 const redis = require('redis')
 
-
 let subscriber = null
 if(process.env.REDIS_HOST){
   subscriber = redis.createClient({
     host: process.env.REDIS_HOST
+  })
+
+  subscriber.subscribe('kontak')
+
+  subscriber.on('message', (topic, message) => {
+    if(topic === 'kontak'){
+      console.log(JSON.parse(message))
+    }
   })
 }
 
