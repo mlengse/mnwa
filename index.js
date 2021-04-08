6c9300616417b85eebde8fe17e76932635a6a6f4
mlengse <anjangkn@gmail.com>
2021-04-08 11:18:16 +0700

    dfsf

--- index.js
+++ index.js
@@ -83,13 +83,8 @@ schedule('30 12 1 * *', async() => {
             let msg = await bot.handleMessage({message: newMessage})
             if(msg.reply && msg.msg.length){
               bot.spinner.succeed(`${msg.time} dari: ${msg.to.user} isi: ${msg.isi} balas: ${msg.msg.split('\n').join(' ')}`)
-              // console.log('')
-              // console.log('-------------')
-              // console.log(msg.msg)
-              // console.log(`-----------`)
-    
             } else {
-              console.log(msg)
+              console.error(`${new Date()} ${JSON.stringify(msg)}`)
             }
       
           }
@@ -107,13 +102,8 @@ schedule('30 12 1 * *', async() => {
         let msg = await bot.handleMessage({message})
         if(msg.reply && msg.msg.length){
           bot.spinner.succeed(`${msg.time} dari: ${msg.to} isi: ${msg.isi} balas: ${msg.msg.split('\n').join(' ')}`)
-          // console.log('')
-          // console.log('-------------')
-          // console.log(msg.msg)
-          // console.log(`-----------`)
-
         } else {
-          console.log(msg)
+          console.error(`${new Date()} ${JSON.stringify(msg)}`)
         }
       }
     })
