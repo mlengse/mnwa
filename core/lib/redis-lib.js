baa8c64b448d961f1538a0266b18d8dd9f867c53
mlengse <anjangkn@gmail.com>
2021-04-08 08:31:49 +0700

    asdasd

--- core/lib/redis-lib.js
+++ core/lib/redis-lib.js
@@ -10,39 +10,42 @@ if(process.env.REDIS_HOST){
 
 exports._getSubscriber = async ({ that }) => {
 
-  subscriber.on('message', async (topic, message) => {
-    let {
-      nama,
-      from,
-      text
-    } = JSON.parse(message)
-    if(topic === 'sendwa'){
-      that.spinner.succeed(`---------------`)
-      that.spinner.succeed(`on new send wa request`)
-    
-      chat = await that.client.checkNumberStatus(from);
-      let profile = await that.client.getNumberProfile(from);
-    
-      if(chat && chat.canReceiveMessage) {
-        await that.addContact({ contact: {
-          nama,
-          from,
-          chat,
-          profile,
-        }})
-    
-        text = `${text}\n${process.env.FORM_LINK ? `Mohon kesediaannya untuk dapat mengisi form kepuasan pelanggan berikut:\n${process.env.FORM_LINK}\n`: ''} ${process.env.ESO_LINK ? `Efek samping dan alergi obat serta pertanyaan/konseling farmasi dapat disampaikan melalui form berikut:\n ${process.env.ESO_LINK}\n` : ''}`
-    
-        await that.client.sendText( from, text)
-        that.spinner.succeed(`${that.getTglDaftarHariIni()} send text to: ${from}, isi: ${text.split('\n').join(' ')}`)
-      } else {
-        that.spinner.succeed(`${that.getTglDaftarHariIni()} ${from} doesn't exists`)
+  if(subscriber) {
+    subscriber.on('message', async (topic, message) => {
+      let {
+        nama,
+        from,
+        text
+      } = JSON.parse(message)
+      if(topic === 'sendwa'){
+        that.spinner.succeed(`---------------`)
+        that.spinner.succeed(`on new send wa request`)
+      
+        chat = await that.client.checkNumberStatus(from);
+        let profile = await that.client.getNumberProfile(from);
+      
+        if(chat && chat.canReceiveMessage) {
+          await that.addContact({ contact: {
+            nama,
+            from,
+            chat,
+            profile,
+          }})
+      
+          text = `${text}\n${process.env.FORM_LINK ? `Mohon kesediaannya untuk dapat mengisi form kepuasan pelanggan berikut:\n${process.env.FORM_LINK}\n`: ''} ${process.env.ESO_LINK ? `Efek samping dan alergi obat serta pertanyaan/konseling farmasi dapat disampaikan melalui form berikut:\n ${process.env.ESO_LINK}\n` : ''}`
+      
+          await that.client.sendText( from, text)
+          that.spinner.succeed(`${that.getTglDaftarHariIni()} send text to: ${from}, isi: ${text.split('\n').join(' ')}`)
+        } else {
+          that.spinner.succeed(`${that.getTglDaftarHariIni()} ${from} doesn't exists`)
+        }
+      
       }
-    
-    }
-  })
-
-  subscriber.subscribe('sendwa')
+    })
+  
+    subscriber.subscribe('sendwa')
+  
+  }
 
   return subscriber
 }
