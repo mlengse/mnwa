dcf4ef977e2e63ac7109d28a13022eb2f0505186
mlengse <anjangkn@gmail.com>
2021-05-31 07:21:07 +0700

    jhghj

--- core/lib/venom-lib.js
+++ core/lib/venom-lib.js
@@ -11,5 +11,22 @@ exports._init = async ({ that }) => {
 
   that.client = await wa.create()
 
+  // jare nggo ngatasi Error: Evaluation failed: TypeError: Cannot read property 'filter' of undefined
+
+  // client.onStreamChange((state) => {
+  //   console.log('Connection status: ', state);
+  //   clearTimeout(time);
+  //   if(state === 'CONNECTED'){
+  //    start(client);
+  //   }
+  //  //  DISCONNECTED when the mobile device is disconnected
+  //   if (state === 'DISCONNECTED' || state === 'SYNCING') {
+  //     time = setTimeout(() => {
+  //       client.close();
+  //      // process.exit(); //optional function if you work with only one session
+  //     }, 80000);
+  //   }
+  // })
+
   return that.client
 }
